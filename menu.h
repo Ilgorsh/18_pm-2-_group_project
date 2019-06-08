62a178b1f945460071befe9dc2bd055245078fec
McAndyLarkin <51488300+McAndyLarkin@users.noreply.github.com>
2019-06-08 12:09:29 +0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,32 +1,29 @@
-#include <sorter.h>
-/* Должны подключиться: #include <QWidget>
- *                      #include <QLabel>
- *                      #include <QKeyEvent>
- * и другие для работы с кнопочками и прочим. Надо пересоздать проект в QT Widget
- */
+#include <manager.h>
 
 #ifndef MENU_H
 #define MENU_H
-class Menu:protected Sorter{
+class Menu:protected Sorter{//Все здешние методы на - самом деле лишь посредники между граф. оболочкой и 
+                                //методами управляющего класса "manager"(бывш sorter)
+                                //Возможно, вообще удалим этот класс, если в нем не появится нужда
 private:
-
 public:
-    void add_plant();//Функция добавления завода, внутри в конце запускается Sorter с новыми данными (прогоняет всё по-новой)
-    void add_container();//Функция добавления контейнера, внутри в конце запускается Sorter с новыми данными (прогоняет всё по-новой)
-    void print_list();//Выводит список заводов с прикреплёнными контейнерами
-    void delete_plant(set<Plant> plants_set, int id); //Функция, удаляющая завод
-    void delete_container(set<Container> containers_set, int id); //Функция, удаляющая контейнер
-    void start_menu(); //Запускает графическое приложение с основным меню
-    void change(int id); //Позволяет редактировать информацию какого-либо объекта по его ID
-
-/* Вся работа с кнопочками, окошками, графикой будет вызываться внутри start_menu()
- * Сами эти функции будут описаны в "mainwindow.cpp" (файл создастся при создании проекта Qt Widget)
- * Объявлены будут в "mainwindow.h" (так же создатся при создании проекта)
- */
+    int add_object(int x, int y, bool *materials, string name = "none", float cost = -1.0);//Метод возвращает индекс, который присуждается ему в manager-е
+                                    //И потом по этому индексу он вызывается, и заполняется его material_list
+    Plant &GetPlant(int id);
+    Container &GetContainer(int id);
+    void delete_plant(int id); //Функция, удаляющая завод
+    void delete_container(int id); //Функция, удаляющая контейнер
+    void Change(int id, int x, int y, bool *materials, string name = "none", float cost = -1.0);//Редакторвание
+    bool CheckPassword(string, bool or);//Проверяет пароль
+    map<int, Plant> &GetPlantMap();         //Читай след-ю строчку
+    map<int, Container> &GetComtainerMap(); //Возможно вместо этих двух методов стоит сделать множество всех id, 
+                                                            //и получать объекты через геттеры
+                                                            //Подумаем
+    void Savetofile(string);
+    void Loadfromfile(string);
+    
 
     Menu();
 };
 
 #endif // MENU_H
-
-//Если останется время, то сделать графическую оболочку
